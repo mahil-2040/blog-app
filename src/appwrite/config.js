cd780f1cc4c9f062b32230641401a219d25c13c7
Mahil <mahilpatel0808@gmail.com>
2025-06-10 10:11:28 +0530

    implemented Appwrite service for post and file management

---
+++ src/appwrite/config.js
@@ -0,0 +1,133 @@
+import conf from "../conf/conf";
+import { Client, Databases, ID, Storage, Query } from "appwrite";
+
+
+export class Service {
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userID}) {
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseID,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userID
+                }
+            );
+        } catch (error) {
+            console.log("Error creating post: ", error);
+        }
+    }
+
+    async updatePost(slug, {title, content, featuredImage, status}) {
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseID,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            );
+        } catch (error) {
+            console.log("Error updating post: ", error);
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseID,
+                conf.appwriteCollectionId,
+                slug,
+            );
+            return true;
+        } catch (error) {
+            console.log("Error deleting post: ", error);
+            return false;
+        } 
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseID,
+                conf.appwriteCollectionId,
+                slug
+            );
+        } catch (error) {
+            console.log("Error fetching post: ", error);
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseID,
+                conf.appwriteCollectionId,
+                queries
+            );
+        } catch (error) {
+            console.log("Error fetching posts: ", error);
+            return false;
+        }
+    }
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            );
+        } catch (error) {
+            console.log("Error uploading file: ", error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            );
+            return true;
+        } catch (error) {
+            console.log("Error deleting file: ", error);
+            return false;
+        }
+    }
+
+    async getFilePreview(fileId) {
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId,
+        );
+    }
+}
+
+
+const service = new Service();
+
+export default service;
