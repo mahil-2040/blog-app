a1dbac30f348eb259fe06522342662f5d97b71e2
Mahil <mahilpatel0808@gmail.com>
2025-06-11 11:16:41 +0530

    implemented core components including Header, Footer, Login, Signup, and PostCard with routing and authentication

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { logout } from '../../store/authSlice.js'
+import authService from '../../appwrite/auth.js'
+
+export function LogoutBtn() {
+
+    const dispatch = useDispatch()
+    const logoutHandler = () => {
+        authService.logout().then(() => {
+            dispatch(logout())
+        })
+    }
+    return (
+        <button
+        className='inline bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+        >
+            Logout
+        </button>
+    )
+}
